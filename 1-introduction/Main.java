3bcfd2911a939360e725c04fc943f370708a7f2a
Michael Torres <mrmojorisen0@gmail.com>
2026-03-26 20:13:39 -0400

    Fixing a typo, and then adding new text.

--- 1-introduction/Main.java
+++ 1-introduction/Main.java
@@ -10,6 +10,8 @@ public class Main {
                 """);
         goalsOfThisCourse();
         whatIsAProgram();
+        compilation();
+        execution();
     }
 
     // --------------------------------------------------------------------------------------------
@@ -20,7 +22,7 @@ public class Main {
                 ---------------------------------------------------------------------------
 
                 The goals of this course is to introduce software engineering. I have
-                chosen to do this through the language java because it forces us to use the
+                chosen to do this through the language Java because it forces us to use the
                 same language we will be using in FRC. There are simpler programming
                 languages syntactically, like C, but the priority of this course is
                 preparation for the competition.
@@ -29,8 +31,9 @@ public class Main {
                 ways to structure simple programs. We will go over the various memory
                 mechanisms available to us: we will go over the stack vs. static vs. heap
                 memory. We will also go over the kind of data that we can use. We will go
-                over things that will lay a decent foundation--you'd learn more foundations
-                in C--though Java is a good place to start.
+                over error handling through Exceptions. We will go over things that will
+                lay a decent foundation--you'd learn more foundations in C--though Java is
+                a good place to start.
 
                 This course will adhere to some of the standards of the Java community. All
                 defined objects--and therefore file names--will begin with an uppercase
@@ -69,6 +72,66 @@ public class Main {
                 easily understood by both ourselves and others. This is why we have 
                 Assembly, Java, C, and the thousands of other languages. So, a program is a
                 set of instuctions for the computer and for humans.
+                """);
+    }
+
+    // --------------------------------------------------------------------------------------------
+    public static void compilation() {
+        out.println("""
+                ---------------------------------------------------------------------------
+                                              Compilation
+                ---------------------------------------------------------------------------
+
+                How does our code that we write--in our case Java--become something that
+                the computer can understand? There is a program called a \"compiler\" that
+                turns our Java source code into what the computer can understand. The Java
+                compiler first turns the Java we wrote into what is called \"JVM (java-
+                virtual-machine) bytecode.\" This bytecode is the final output of the
+                compiler: though, then, how does this get executed by the computer because
+                the computer does not understand jvm bytecode? There is a middle program,
+                called the JRE (the Java Runtime Environment) that constantly runs
+                whenever a Java program is being run. The JRE is a \"translator\" that
+                turns the pre-compiled JVM bytecode into machine code for the computer
+                that it is being run on. This is what enables compiled Java code to be able
+                to be run on any type of machine that has a built JRE.
+
+                This is a huge advantage over fully compiled languages, like C, where you
+                have to compile for the machine you want to run on. This one thing enabled
+                the website revolution in the late 90's and early 2000's.
+
+                Now, what does the Java compiler do other than make JVM bytecode; or
+                really, how does the compiler go from Java source code to JVM bytecode? I
+                am really going to only give a broad-stroke overview, what the compiler
+                does is quite a bit with many internal steps. First, it creates
+                references to the imported code defined in the beginning of the file. That
+                is used in the type-checking process: which is next. After that, it parses
+                your code for any syntax issues. Basically after that point, it creates
+                the JVM bytecode for that particular file, which includes references to
+                the imported classes.
+                """);
+    }
+
+    // --------------------------------------------------------------------------------------------
+    public static void execution() {
+        out.println("""
+                ---------------------------------------------------------------------------
+                                      How Does a Program Execute? 
+                ---------------------------------------------------------------------------
+
+                Code executes one step at a time. This is most often one line at a time:
+                there are certain exceptions to this that'll we'll go over later. The
+                program starts in the \"Main\" class' \"main\" function. The main function
+                in Java always looks exactly the same as I have it written above. There is
+                a lot of syntax in just the main function's definition that I do not expect
+                or even want you to know yet--this is one of those things where Java's
+                syntax is a bit heavy for beginners that we have to wave away.
+
+                The lines inside the main function are then executed one line at a time.
+                Any function calls, like println() or clear() or goalsOfThisCourse(), are
+                \"called\" in order inside of main. When a function is called, the program
+                then \"moves\" into that function being called and the steps continue. Once
+                the end of the called function reaches its end, like lets say after
+                goalsOfThisCourse() calls println(), the end of the function is reached.
                 """);
     }
 
