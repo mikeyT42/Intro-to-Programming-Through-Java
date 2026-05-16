1386897274e74dc3a368c37e8739ab6b23a77f91
Michael Torres <mrmojorisen0@gmail.com>
2026-05-16 01:43:22 -0400

    Finished the logical operators, moving onto the control structues. Currently there are errors.

--- 3-logic-operators-and-control-structures/Main.java
+++ 3-logic-operators-and-control-structures/Main.java
@@ -11,6 +11,7 @@ public class Main {
                 """);
 
         logicalOperators();
+        controlStructures();
     }
 
     // --------------------------------------------------------------------------------------------
@@ -45,6 +46,10 @@ public class Main {
 
         theOrOperator();
         theAndOperator();
+        theEqualsToOperator();
+        theNotOperator();
+        theNotEqualsOperator();
+        theRelationalOperators();
     }
 
     // --------------------------------------------------------------------------------------------
@@ -132,6 +137,7 @@ public class Main {
     }
 
     // --------------------------------------------------------------------------------------------
+    @SuppressWarnings("all")
     public static void theEqualsToOperator() {
         out.println("""
                 ---------------------------------------------------------------------------
@@ -185,6 +191,238 @@ public class Main {
                             The NOT (!) Operator
                 ---------------------------------------------------------------------------
                 """);
+
+        out.println("""
+                The NOT operator is also pretty simple. All it does is flip the logical
+                value. So, if something is true it is now false (not true) and something
+                that is false is now true (not false). Unlike the == operator it will only
+                work on primitive types, so it cannot be used for objects and can only be
+                used on their data members. Using the ! operator on arrays (not the
+                array element) will behave like an object.
+
+                            |   A   |  !A   |
+                            |-------|-------|
+                            | false | true  |
+                            | true  | false |
+                """);
+
+        out.printf("!true = %i\n", !true);
+        out.printf("!false = %i\n", !false);
+        out.printf("!(5 == 10) = %i\n", !(5 == 10));
+        Object o = null;
+        Object o2 = new Object();
+        out.printf("!(o == null) = %i\n", !(o == null));
+        out.printf("!(o2 == null) = %i\n", !(o2 == null));
+        int[] a = new int[10];
+        out.printf("!(a == null) = %i\n", !(a == null));
+    }
+
+    // --------------------------------------------------------------------------------------------
+    @SuppressWarnings("all")
+    public static void theNotEqualsOperator() {
+        out.println("""
+                ---------------------------------------------------------------------------
+                            The \"Is Not Equal To\" (!=) Operator
+                ---------------------------------------------------------------------------
+                """);
+
+        out.println("""
+                Comparing if something is not equal to something else is quite common in
+                programming, so common that the Java language desingers decided to make a
+                shortcut for !(A == B). To do this, they introduced != to the logical
+                operator list. All it simply does is invert the operation of the ==
+                operator.
+
+                            |   A   |   B   | A != B |
+                            |-------|-------|--------|
+                            | false | false | false  |
+                            | false | true  | true   |
+                            | true  | false | true   |
+                            | true  | true  | false  |
+                """);
+
+        out.printf("false != false = %i\n", false != false);
+        out.printf("true != false = %i\n", true != false);
+        out.printf("2 != 5 = %i\n", 2 != 5);
+        out.printf("10 != 10 = %i\n", 10 != 10);
+    }
+
+    // --------------------------------------------------------------------------------------------
+    public static void theRelationalOperators() {
+        out.println("""
+                ---------------------------------------------------------------------------
+                            The \"Relational\" (<, >, <=, >=) Operators
+                ---------------------------------------------------------------------------
+                """);
+
+        out.println("""
+                These operators compare values to each other and don't really have truth
+                tables associated to them. Basically, they do what is written on their
+                respective boxes. The < operator asks if the left value is less than the
+                right value; the > operator asks if the left value is greater than the
+                right value; the <= operator asks if the left value is less than or equal
+                to the right value; and the >= operator asks if the left value is greater
+                than or equal to the right value. Depending on the relational comparison,
+                true or false is output.
+                """);
+
+        out.printf("1 > 2 = %i\n", 1 > 2);
+        out.printf("1 < 2 = %i\n", 1 < 2);
+        out.printf("1 <= 2 = %i\n", 1 <= 2);
+        out.printf("1 >= 2 = %i\n", 1 >= 2);
+        out.printf("3 >= 3 = %i\n", 3 >= 3);
+    }
+
+    // --------------------------------------------------------------------------------------------
+    public static void controlStructures() {
+        out.println("""
+                ---------------------------------------------------------------------------
+                        The Control Structures
+                ---------------------------------------------------------------------------
+                """);
+
+        out.println("""
+                Control structures take advantage of logical operators to conditionally
+                control the execution of certain code blocks.
+                """);
+
+        ifStatements();
+        switchStatements();
+        while_loops();
+        for_loops();
+        do_while_loops();
+        tryCatchFinally();
+    }
+
+    // --------------------------------------------------------------------------------------------
+    public static void ifStatements() {
+        out.println("""
+                ---------------------------------------------------------------------------
+                            The \"if\" Statement
+                ---------------------------------------------------------------------------
+                """);
+
+        out.println("""
+                \"if\" statements control the flow of code execution. Depending on their
+                resolution, certain blocks of code can execute. This allows you, the
+                developer, to give conditional logic to your program; this allows your
+                program to make decisions that you allow. Your program can \"think.\"
+                """);
+
+        if (true)
+            out.println("I am true");
+
+        if (false)
+            out.println("I won't execute because the if will resolve to false.");
+        else
+            out.println("""
+                    I will be executed because my brother \"if\" resolved to false\n"
+                    and now I will execute."
+                    """);
+
+        if (false)
+            out.println("I won't execute because the if will resolve to false.");
+        else if (true)
+            out.println("""
+                    I will execute because I have my own \"if\" that resolves to true.
+                    """);
+        else
+            out.println("I won't execute because my brother \"if\" resolved to true.");
+
+        if (false) {
+            out.println("I won't execute because the if will resolve to false.");
+        } else if (false) {
+            out.println("I will not execute because my \"if\" resolved to false.");
+        } else {
+            out.println("""
+                    I will execute because all the other \"if\" statements failed to
+                    resolve to true; so, it defaults to me.
+                    """);
+        }
+    }
+    
+    // --------------------------------------------------------------------------------------------
+    public static void switchStatements() {
+        out.println("""
+                ---------------------------------------------------------------------------
+                            The \"switch\" Statement
+                ---------------------------------------------------------------------------
+                """);
+
+  out.println("""
+       The \"switch\" statement is a simplified series of \"if\" statements.
+       It will only work on numbers types and matches exactly on that
+       number, so it is not exactly useful for floating point types. It will
+       work on integers types and enums, and technically works on boolean
+       types; though, it is not as useful for that. It also works on the
+       char data type since characters are just numbers mapped to
+       characters. Really, it's purpose is to test a series of things and
+       then execute certain code.
+       """);
+
+  int i = 1;
+  switch (i) {
+  case 1:
+    out.println("i is 1.");
+    break;
+  case 2:
+    puts("i is 2; which won't happen in this case.");
+    break;
+  default:
+    puts("The default case, we'll get to this in a second.");
+    break;
+  }
+
+  puts("\nAs you can see, we use the \"break\" statement at the end of each\n"
+       "\"case\" block. This is important. If we don't do this, then the next\n"
+       "\"case\" block is executed until we hit a \"break\". Let me show\n"
+       "that.\n");
+
+  int d = 1;
+  switch (d) {
+  case 1:
+    printf("case 1 :: d = %i\n", d);
+  case 2:
+    printf("case 2 :: d = %i\n", d);
+    break;
+  default:
+    puts("The default case, we'll get to this in a second.");
+    break;
+  }
+
+  puts("\nThe \"default\" case is there for when no other case can be\n"
+       "executed because none of the \"case\" statements matched. It is\n"
+       "particularly useful when handling errors.\n");
+
+  char c = 'z';
+  switch (c) {
+  case 'a':
+    puts("case a.");
+    break;
+  case 'b':
+    puts("case b.");
+    break;
+  default:
+    printf("default case :: c = %c\n", c);
+    break;
+  }
+
+  puts("\nThe \"switch\" statement is especially useful when used with\n"
+       "enums!\n");
+
+  enum Color { RED, GREEN, BLUE };
+  enum Color color = BLUE;
+  switch (color) {
+  case RED:
+    puts("Execute RED code.");
+    break;
+  case GREEN:
+    puts("Execute GREEN code.");
+    break;
+  case BLUE:
+    puts("Execute BLUE code.");
+    break;
+  }
     }
 
     // --------------------------------------------------------------------------------------------
